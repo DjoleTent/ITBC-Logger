dad00d32893809092ea75e83d47c3b422e7c007d
Djordje <djordjedjordjevic2@gmail.com>
2022-10-20 13:52:27 +0200

    First commit, clients in base

--- src/main/java/com/example/ITBC/Logger/ItbcLoggerApplication.java
+++ src/main/java/com/example/ITBC/Logger/ItbcLoggerApplication.java
@@ -8,6 +8,7 @@ public class ItbcLoggerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItbcLoggerApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
